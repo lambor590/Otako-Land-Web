9afdc78adecbacbcbcfc61949fba4f237d59ea28
lambor590 <lambor590@gmail.com>
2023-05-26 16:16:53 +0200

    Optimizar

--- holaquetal.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("erboton").addEventListener("click", function () {
-  alert("Otako Craft Mods ha cerrado (no para siempre).\nEstáte atento a esta página, próximamente habrán noticias.")
-});
